b73484ea95c2dfa7ac167a3873e730b0d0a60658
Simon Lund <simon97lund@gmail.com>
2020-10-11 22:40:24 +0200

    comment out used configuration

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ DENYLIST = []
 
 
 # Example
-DOMAIN = "org.sosy_lab.cpachecker"
-DOTFILE_PATH = "./data/cpachecker.jar.dot"
-DENYLIST = [
-        "org.sosy_lab.cpachecker.cpa.smg.graphs.object.test.",
-        "org.sosy_lab.cpachecker.util.test.",
-        "org.sosy_lab.cpachecker.cpa.policyiteration.tests.",
-        "org.sosy_lab.cpachecker.cpa.arg.witnessexport.WitnessExporterTest$",
-        "org.sosy_lab.cpachecker.cpa.automaton.AutomatonInternalTest$",
-        "org.sosy_lab.cpachecker.cpa.invariants.InvariantsTestSuite",
-    ]
+# DOMAIN = "org.sosy_lab.cpachecker"
+# DOTFILE_PATH = "./data/cpachecker.jar.dot"
+# DENYLIST = [
+#         "org.sosy_lab.cpachecker.cpa.smg.graphs.object.test.",
+#         "org.sosy_lab.cpachecker.util.test.",
+#         "org.sosy_lab.cpachecker.cpa.policyiteration.tests.",
+#         "org.sosy_lab.cpachecker.cpa.arg.witnessexport.WitnessExporterTest$",
+#         "org.sosy_lab.cpachecker.cpa.automaton.AutomatonInternalTest$",
+#         "org.sosy_lab.cpachecker.cpa.invariants.InvariantsTestSuite",
+#     ]
